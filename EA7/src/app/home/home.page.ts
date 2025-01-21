80ea7265e71d2fdab9dc16df9206b6c3d19c0c3b
Omar Del Toro <19033@portalcedi.edu.mx>
2025-01-21 11:38:27 -0600

    final usuarios

--- EA7/src/app/home/home.page.ts
+++ EA7/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,50 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
+  usuario = this.fb.group({
+    nombre: ['', [Validators.required, Validators.minLength(1)]],
+    apellido: ['', [Validators.required, Validators.minLength(1)]],
+    email: ['', [Validators.email]],
+    edad: ['', [Validators.min(18)]],
+    username: ['', [Validators.required, Validators.minLength(4)]],
+    aceptaTerminos: [false, [Validators.requiredTrue]],
+  });
 
-  constructor() {}
+  usuarios: any[] = [];
+  indiceEdicion: number | null = null; 
+  constructor(private fb: FormBuilder) {}
 
+  guardarDatos() {
+    if (this.usuario.valid) {
+      if (this.indiceEdicion === null) {
+        this.usuarios.push(this.usuario.value);
+      } else {
+        this.usuarios[this.indiceEdicion] = this.usuario.value;
+        this.indiceEdicion = null; 
+      }
+
+      this.usuario.reset(); 
+    }
+  }
+
+  editarUsuario(index: number) {
+    const usuario = this.usuarios[index];
+    this.usuario.setValue({
+      nombre: usuario.nombre,
+      apellido: usuario.apellido,
+      email: usuario.email,
+      edad: usuario.edad,
+      username: usuario.username,
+      aceptaTerminos: usuario.aceptaTerminos,
+    });
+    this.indiceEdicion = index; 
+  }
+
+  eliminarUsuario(index: number) {
+    this.usuarios.splice(index, 1); 
+    if (this.indiceEdicion === index) {
+      this.usuario.reset(); 
+      this.indiceEdicion = null;
+    }
+  }
 }
