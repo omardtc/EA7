018f00a17e9f3c3ef4bc6e225cdafaeda2ebaf07
Omar Del Toro <19033@portalcedi.edu.mx>
2025-01-21 08:35:02 -0600

    final agenda

--- EA7/src/app/home/home.page.ts
+++ EA7/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,33 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  fecha = new FormControl('');
+  nombre = new FormControl('');
+  descripcion = new FormControl('');
+
+  tareas: {nombre: string, fecha: string, descripcion: string|null, completada: boolean}[] = [];
+
   constructor() {}
 
+  NewTarea(){
+    if (!this.descripcion.value)this.descripcion.setValue('...');
+    if (this.fecha.value && this.nombre.value){
+      const NewTarea = {
+        nombre: this.nombre.value,
+        fecha: this.fecha.value,
+        descripcion: this.descripcion.value,
+        completada: false
+      };
+
+      this.tareas.push(NewTarea);
+
+      this.nombre.setValue('');
+      this.fecha.setValue('');
+      this.descripcion.setValue('');
+    }else{
+      alert('Debe llenar los espacios indicados');
+    }
+  }
+
+  
 }
