96e961cc3128454374036bfc167d5946d3378ab7
Omar Del Toro <19033@portalcedi.edu.mx>
2025-01-23 09:11:20 -0600

    final eventos

--- EA7/src/app/home/home.page.ts
+++ EA7/src/app/home/home.page.ts
@@ -10,20 +10,32 @@ import { FormControl } from '@angular/forms';
 export class HomePage {
 
   fecha = new FormControl('');
+  fechafin = new FormControl('');
   nombre = new FormControl('');
   descripcion = new FormControl('');
+  encargado = new FormControl('');
+  telefono = new FormControl('');
+  ubicacion = new FormControl('');
 
-  tareas: {nombre: string, fecha: string, descripcion: string|null, completada: boolean}[] = [];
+  tareas: {nombre: string, fecha: string, fechafin: string, descripcion: string|null, encargado: string|null, telefono: string|null, ubicacion: string, completada: boolean}[] = [];
 
   constructor() {}
 
   NewTarea(){
     if (!this.descripcion.value)this.descripcion.setValue('...');
-    if (this.fecha.value && this.nombre.value){
+    if (!this.encargado.value)this.encargado.setValue('...');
+    if (!this.telefono.value)this.telefono.setValue('...');
+
+
+    if (this.fecha.value && this.nombre.value && this.fechafin.value && this.ubicacion.value){
       const NewTarea = {
         nombre: this.nombre.value,
         fecha: this.fecha.value,
+        fechafin: this.fechafin.value,
         descripcion: this.descripcion.value,
+        encargado: this.encargado.value,
+        telefono: this.telefono.value,
+        ubicacion: this.ubicacion.value,
         completada: false
       };
 
@@ -32,6 +44,10 @@ export class HomePage {
       this.nombre.setValue('');
       this.fecha.setValue('');
       this.descripcion.setValue('');
+      this.fechafin.setValue('');
+      this.encargado.setValue('');
+      this.telefono.setValue('');
+      this.ubicacion.setValue('');
     }else{
       alert('Debe llenar los espacios indicados');
     }
