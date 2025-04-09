05e3a83cceaee9c2b72b99c52328d639d308c5c2
Omar Del Toro <19033@portalcedi.edu.mx>
2025-04-08 22:21:26 -0600

    4 plugins

--- EA7/src/app/home/home.page.ts
+++ EA7/src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import {Camera, CameraResultType} from '@capacitor/camera';
+import { Device } from '@capacitor/device';
+import { Geolocation } from '@capacitor/geolocation';
+import { Network } from '@capacitor/network';
 
 @Component({
   selector: 'app-home',
@@ -10,4 +14,46 @@ export class HomePage {
 
   constructor() {}
 
+  
+
+  takePhoto = async () => {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.DataUrl,
+    });
+    console.log(image.dataUrl);
+};
+
+  logDeviceInfo = async () => {
+    const info = await Device.getInfo();
+    console.log(info);
+  };
+
+  logBatteryInfo = async () => {
+    const batteryInfo = await Device.getBatteryInfo();
+    console.log(batteryInfo);
+  };
+
+  logCurrentNetworkStatus = async () => {
+    const status = await Network.getStatus();
+  
+    console.log('Network status:', status);
+  };
+
+  logCurrentPosition = async () => {
+    try {
+      const coordinates = await Geolocation.getCurrentPosition();
+      console.log('Current position:', coordinates);
+    } catch (error) {
+      console.error('Error getting location:', error);
+    }
+  };
+
+  checkPermissions = async () => {
+    const permission = await Geolocation.requestPermissions();
+    console.log('Geolocation permission:', permission);
+  };
+  
+  
 }
